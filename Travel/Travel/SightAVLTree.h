9673491710e7ee8f0cdedbcf6df924b173e67bbd
dccif <heyujieboy@163.com>
2017-07-04 11:36:24 +0800

    AVL Tree version

--- Travel/Travel/SightAVLTree.h
+++ Travel/Travel/SightAVLTree.h
@@ -1,6 +1,8 @@
 #pragma once
-#include"Guestchain.h"
+#include<iomanip>
+#include<iostream>
 #include<string>
+#include"Guestchain.h"
 
 using namespace std;
 
@@ -16,7 +18,7 @@ public:
 	int pv;
 
 	//构造函数
-	sightAVLTreeNode(int newid, string newname, sightAVLTreeNode *l, sightAVLTreeNode *r) :sightID(newid), sightName(newname), lchild(l), rchild(r) {}
+	sightAVLTreeNode(int newid, string newname, sightAVLTreeNode *l, sightAVLTreeNode *r) :sightID(newid), sightName(newname), chain(NULL), lchild(l), rchild(r), flow(0), pv(0) {}
 };
 
 //景点的二叉排序树
@@ -30,11 +32,17 @@ public:
 
 	//遍历树
 	void preOrder();
+	void preOrderPV();
+	void preOrderAll();
+
 	void inOrder();
+
 	void postOrder();
 
 	//查找
 	sightAVLTreeNode* search(int newid);	//递归
+	int minimun();
+	int maximun();
 
 	//插入
 	void insert(int newid, string newname);
@@ -45,15 +53,21 @@ public:
 	void print();
 
 private:
-	int TreeHeight(sightAVLTreeNode* tree);
+	int height(sightAVLTreeNode* tree);
 
 	//遍历
 	void preOrder(sightAVLTreeNode *tree);
+	void preOrderPV(sightAVLTreeNode *tree);
+	void preOrderAll(sightAVLTreeNode *tree);
+
 	void inOrder(sightAVLTreeNode *tree);
+
 	void postOrder(sightAVLTreeNode *tree);
 
 	//查找
 	sightAVLTreeNode* search(int newid, sightAVLTreeNode *x);
+	sightAVLTreeNode* minimun(sightAVLTreeNode* tree);
+	sightAVLTreeNode* maximun(sightAVLTreeNode* tree);
 
 	//旋转
 	sightAVLTreeNode* LLRotate(sightAVLTreeNode* k2);
@@ -62,7 +76,7 @@ private:
 	sightAVLTreeNode* RLRotate(sightAVLTreeNode* k1);
 
 	//插入
-	sightAVLTreeNode* insert(sightAVLTreeNode* &tree, int newid);
+	sightAVLTreeNode* insert(sightAVLTreeNode* &tree, int newid, string newname);
 
 	//删除
 	sightAVLTreeNode* remove(sightAVLTreeNode* &tree, sightAVLTreeNode* z);
@@ -72,7 +86,7 @@ private:
 	void destory(sightAVLTreeNode* &tree);
 };
 
-int sightAVLTree::TreeHeight(sightAVLTreeNode* tree)
+int sightAVLTree::height(sightAVLTreeNode* tree)
 {
 	if (tree != NULL) {
 		return tree->height;
@@ -81,7 +95,7 @@ int sightAVLTree::TreeHeight(sightAVLTreeNode* tree)
 }
 
 int sightAVLTree::height() {
-	return TreeHeight(TreeRoot);
+	return height(TreeRoot);
 }
 
 int sightAVLTree::max(int a, int b) {
@@ -97,9 +111,37 @@ void sightAVLTree::preOrder(sightAVLTreeNode* tree) {
 	}
 }
 
+void sightAVLTree::preOrderPV(sightAVLTreeNode* tree) {
+	if (tree != NULL) {
+		cout << tree->sightID << " 名称："<<tree->sightName<<" 预约人数为：" << tree->pv << endl;
+		preOrderPV(tree->lchild);
+		preOrderPV(tree->rchild);
+	}
+}
+
+void sightAVLTree::preOrderAll(sightAVLTreeNode* tree) {
+	if (tree != NULL) {
+		cout << tree->sightID << " 名称：" << tree->sightName << " 预约人数为：" << tree->pv << endl;
+		tree->chain->printall();
+		preOrderAll(tree->lchild);
+		preOrderAll(tree->rchild);
+	}
+}
+
+
+
 void sightAVLTree::preOrder() {
 	preOrder(TreeRoot);
 }
+
+void sightAVLTree::preOrderPV() {
+	preOrderPV(TreeRoot);
+}
+
+void sightAVLTree::preOrderAll() {
+	preOrderAll(TreeRoot);
+}
+
 //中序遍历
 void sightAVLTree::inOrder(sightAVLTreeNode* tree) {
 	if (tree != NULL) {
@@ -125,4 +167,249 @@ void sightAVLTree::postOrder() {
 	postOrder(TreeRoot);
 }
 
+//查找景点
+sightAVLTreeNode * sightAVLTree::search(int newid, sightAVLTreeNode * x)
+{
+	if (x == NULL || x->sightID == newid)
+		return x;
+	if (newid < x->sightID)
+		return search(newid, x->lchild);
+	else
+		return search(newid, x->rchild);
+}
 
+sightAVLTreeNode * sightAVLTree::search(int newid)
+{
+	return search(newid, TreeRoot);
+}
+
+sightAVLTreeNode * sightAVLTree::minimun(sightAVLTreeNode * tree)
+{
+	if (tree == NULL)
+		return NULL;
+
+	while (tree->lchild != NULL)
+	{
+		tree = tree->lchild;
+	}
+	return tree;
+}
+
+int sightAVLTree::minimun()
+{
+	sightAVLTreeNode *p = minimun(TreeRoot);
+	if (p != NULL)
+		return p->sightID;
+
+	return (int)NULL;
+}
+
+
+sightAVLTreeNode * sightAVLTree::maximun(sightAVLTreeNode * tree)
+{
+	if (tree == NULL)
+		return NULL;
+
+	while (tree->rchild != NULL)
+	{
+		tree = tree->rchild;
+	}
+	return tree;
+}
+
+int sightAVLTree::maximun() {
+	sightAVLTreeNode* p = maximun(TreeRoot);
+	if (p != NULL)
+		return p->sightID;
+
+	return (int)NULL;
+}
+
+//LL旋转
+sightAVLTreeNode* sightAVLTree::LLRotate(sightAVLTreeNode* k2) {
+	sightAVLTreeNode* k1;
+	k1 = k2->lchild;
+	k2->lchild = k1->rchild;
+	k1->rchild = k2;
+
+	k2->height = max(height(k2->lchild), height(k2->rchild)) + 1;
+	k1->height = max(height(k1->lchild), k2->height) + 1;
+
+	return k1;
+}
+//RR旋转
+sightAVLTreeNode* sightAVLTree::RRRotate(sightAVLTreeNode* k1) {
+	sightAVLTreeNode* k2;
+
+	k2 = k1->rchild;
+	k1->rchild = k2->lchild;
+	k2->lchild = k1;
+
+	k1->height = max(height(k1->lchild), height(k1->rchild)) + 1;
+	k2->height = max(height(k2->rchild), k1->height) + 1;
+
+	return k2;
+}
+//LR旋转
+sightAVLTreeNode* sightAVLTree::LRRotate(sightAVLTreeNode* k3) {
+	k3->lchild = RRRotate(k3->lchild);
+
+	return LLRotate(k3);
+}
+//RL旋转
+sightAVLTreeNode* sightAVLTree::RLRotate(sightAVLTreeNode* k1) {
+	k1->rchild = LLRotate(k1->rchild);
+
+	return RRRotate(k1);
+}
+
+
+//插入节点
+sightAVLTreeNode* sightAVLTree::insert(sightAVLTreeNode* &tree, int newid, string newname)
+{
+	if (tree == NULL)
+	{
+		// 新建节点
+		tree = new sightAVLTreeNode(newid, newname, NULL, NULL);
+		if (tree == NULL)
+		{
+			cout << "ERROR: create avltree node failed!" << endl;
+			return NULL;
+		}
+	}
+	else if (newid < tree->sightID) // 应该将key插入到"tree的左子树"的情况
+	{
+		tree->lchild = insert(tree->lchild, newid, newname);
+		// 插入节点后，若AVL树失去平衡，则进行相应的调节。
+		if (height(tree->lchild) - height(tree->rchild) == 2)
+		{
+			if (newid < tree->lchild->sightID)
+				tree = LLRotate(tree);
+			else
+				tree = LRRotate(tree);
+		}
+	}
+	else if (newid > tree->sightID) // 应该将key插入到"tree的右子树"的情况
+	{
+		tree->rchild = insert(tree->rchild, newid, newname);
+		// 插入节点后，若AVL树失去平衡，则进行相应的调节。
+		if (height(tree->rchild) - height(tree->lchild) == 2)
+		{
+			if (newid > tree->rchild->sightID)
+				tree = RRRotate(tree);
+			else
+				tree = RLRotate(tree);
+		}
+	}
+	else //key == tree->key)
+	{
+		cout << "添加失败：不允许添加相同的节点！" << endl;
+	}
+
+	tree->height = max(height(tree->lchild), height(tree->rchild)) + 1;
+
+	return tree;
+}
+
+void sightAVLTree::insert(int newid, string itsname)
+{
+	insert(TreeRoot, newid, itsname);
+}
+
+//删除节点
+sightAVLTreeNode* sightAVLTree::remove(sightAVLTreeNode* &tree, sightAVLTreeNode* z)
+{
+	// 根为空 或者 没有要删除的节点，直接返回NULL。
+	if (tree == NULL || z == NULL)
+		return NULL;
+
+	if (z->sightID < tree->sightID)        // 待删除的节点在"tree的左子树"中
+	{
+		tree->lchild = remove(tree->lchild, z);
+		// 删除节点后，若AVL树失去平衡，则进行相应的调节。
+		if (height(tree->rchild) - height(tree->lchild) == 2)
+		{
+			sightAVLTreeNode *r = tree->rchild;
+			if (height(r->lchild) > height(r->rchild))
+				tree = RLRotate(tree);
+			else
+				tree = RRRotate(tree);
+		}
+	}
+	else if (z->sightID > tree->sightID)// 待删除的节点在"tree的右子树"中
+	{
+		tree->rchild = remove(tree->rchild, z);
+		// 删除节点后，若AVL树失去平衡，则进行相应的调节。
+		if (height(tree->lchild) - height(tree->rchild) == 2)
+		{
+			sightAVLTreeNode *l = tree->lchild;
+			if (height(l->rchild) > height(l->lchild))
+				tree = LRRotate(tree);
+			else
+				tree = LLRotate(tree);
+		}
+	}
+	else    // tree是对应要删除的节点。
+	{
+		// tree的左右孩子都非空
+		if ((tree->lchild != NULL) && (tree->rchild != NULL))
+		{
+			if (height(tree->lchild) > height(tree->rchild))
+			{
+				// 如果tree的左子树比右子树高；
+				// 则(01)找出tree的左子树中的最大节点
+				//   (02)将该最大节点的值赋值给tree。
+				//   (03)删除该最大节点。
+				// 这类似于用"tree的左子树中最大节点"做"tree"的替身；
+				// 采用这种方式的好处是：删除"tree的左子树中最大节点"之后，AVL树仍然是平衡的。
+				sightAVLTreeNode* max = maximun(tree->lchild);
+				tree->sightID = max->sightID;
+				tree->lchild = remove(tree->lchild, max);
+			}
+			else
+			{
+				// 如果tree的左子树不比右子树高(即它们相等，或右子树比左子树高1)
+				// 则(01)找出tree的右子树中的最小节点
+				//   (02)将该最小节点的值赋值给tree。
+				//   (03)删除该最小节点。
+				// 这类似于用"tree的右子树中最小节点"做"tree"的替身；
+				// 采用这种方式的好处是：删除"tree的右子树中最小节点"之后，AVL树仍然是平衡的。
+				sightAVLTreeNode* min = minimun(tree->rchild);
+				tree->sightID = min->sightID;
+				tree->rchild = remove(tree->rchild, min);
+			}
+		}
+		else
+		{
+			sightAVLTreeNode* tmp = tree;
+			tree = (tree->lchild != NULL) ? tree->lchild : tree->rchild;
+			delete tmp;
+		}
+	}
+
+	return tree;
+}
+
+void sightAVLTree::remove(int newid)
+{
+	sightAVLTreeNode* z;
+
+	if ((z = search(newid, TreeRoot)) != NULL)
+		TreeRoot = remove(TreeRoot, z);
+}
+
+void sightAVLTree::destory(sightAVLTreeNode* &tree)
+{
+	if (tree == NULL)
+		return;
+	if (tree->lchild != NULL)
+		destory(tree->lchild);
+	if (tree->rchild != NULL)
+		destory(tree->rchild);
+
+	delete tree;
+}
+
+void sightAVLTree::destory() {
+	destory(TreeRoot);
+}
--- Travel/Travel/Travel.cpp
+++ Travel/Travel/Travel.cpp
@@ -3,67 +3,80 @@
 #include<random>
 #include"Guestchain.h"
 #include"SightTree.h"
+#include"SightAVLTree.h"
 
 using namespace std;
 
-void AllTreePrint(sightTree* tree) {				//总输出函数
-	cout << "中序遍历为："<<endl;
-	tree->inorder(tree);
-	cout << "前序遍历为：" << endl;
-	tree->preorder(tree);
-}
-
 //界面
 int main() {
-	int UserID;										//登录信息判别
+	int UserID;									//登录信息判别
 	int SightID;									//景点信息
-	int guestRandomId;								//游客信息
-	string GuestName;								//游客姓名
+	int guestRandomId;							//游客信息
+	string GuestName;							//游客姓名
+	string SightName;
 
-	guestchain* headguest;							//当前游客链表头指针，指向当前游客链表的头
-	guestchain* GCoperatepoint;						//当前游客链表的操作指针
+	guestchain* headguest;						//当前游客链表头指针，指向当前游客链表的头
+	guestchain* GCoperatepoint;					//当前游客链表的操作指针
 
-	sightTree* MainSightTreePoint;					//景点树头指针(主存储指针）
-	sightTree* SToperatepoint;						//景点树操作指针
-	sightTree* STSearchpoint;						//景点搜索指针
+	//sightTree* MainSightTreePoint;				//景点树头指针(主存储指针）
+	//sightTree* SToperatepoint;					//景点树操作指针
+	//sightTree* STSearchpoint;					//景点搜索指针
 
-	MainSightTreePoint = new sightTree(50);			//初始化景点树
-	SToperatepoint = MainSightTreePoint;			//将操作指针初始化
-	STSearchpoint = MainSightTreePoint;				//初始化搜索指针
+	//MainSightTreePoint = new sightTree(50);		//初始化景点树
+	//SToperatepoint = MainSightTreePoint;			//将操作指针初始化
+	//STSearchpoint = MainSightTreePoint;			//初始化搜索指针
+
+	sightAVLTree* MainSightAVLTreePoint = new sightAVLTree();
+	sightAVLTreeNode* OperatePoint = NULL;			//结点操作指针
+	sightAVLTreeNode* SearchPoint = NULL;			//结点搜索指针
 
 	bool UILoop = 1;
 	cout << "****************************************" << endl;
-	cout << "*       欢迎使用旅游信息管理系统       *"<<endl;
+	cout << "*       欢迎使用旅游信息管理系统       *" << endl;
 	cout << "****************************************" << endl;		//初始化界面
 	while (UILoop)
 	{
 		cout << "请输入登录身份：（1：管理员，2：游客，3：退出）";
 		cin >> UserID;
+		system("cls");
 		//管理员操作
 		while (UserID == 1) {
 			//输入操作
 			int OperateId;
-			cout << "请输入操作：（1：添加景点代码，2：删除景点，3：查看景点，4：退出）";
+			cout << "请输入操作：（1：添加景点代码，2：删除景点，3：查看景点预约人数，4，查看景点详情，5：退出）";
 			cin >> OperateId;
+			system("cls");
 
 			if (OperateId == 1) {
-				cout << "请输入节点：";
-				cin >> SightID;
-				MainSightTreePoint->insertnode(SightID, SToperatepoint);		//根据输入插入景点树
-				AllTreePrint(MainSightTreePoint);
-				system("cls");
+				cout << "请输入景点代码和名称用空格分割：";
+				cin >> SightID >> SightName;
+				MainSightAVLTreePoint->insert(SightID, SightName);		//根据输入插入景点树
+				//system("cls");
 			}
 			if (OperateId == 2) {
 				cout << "请输入节点：";
 				cin >> SightID;
-				MainSightTreePoint->deletenode(SightID, SToperatepoint);		//根据输入删除景点树结点
-				AllTreePrint(MainSightTreePoint);
+				SearchPoint = MainSightAVLTreePoint->search(SightID);
+				if (SearchPoint->chain != NULL) {
+					string operate1;
+					cout << "当前景点下有游客，确定要删除吗？（Y/N）";
+					cin >> operate1;
+					if (operate1 == "y" || operate1 == "Y") {
+						MainSightAVLTreePoint->remove(SightID);
+					}
+				}
+				//MainSightAVLTreePoint->remove(SightID);
+				//MainSightTreePoint->deletenode(SightID, SToperatepoint);		//根据输入删除景点树结点
+				//AllTreePrint(MainSightTreePoint);
 				system("cls");
 			}
 			else if (OperateId == 3) {
-				AllTreePrint(MainSightTreePoint);
+				MainSightAVLTreePoint->preOrderPV();
 			}
 			else if (OperateId == 4) {
+				MainSightAVLTreePoint->preOrderAll();
+			}
+			else if (OperateId == 5) {
 				break;
 			}
 		}
@@ -81,50 +94,51 @@ int main() {
 			cin >> OperateId;
 
 			if (OperateId == 1) {
-				AllTreePrint(MainSightTreePoint);
+				MainSightAVLTreePoint->preOrderPV();
 			}
 			else if (OperateId == 2) {
 				cout << "请输入姓名：";
 				cin >> GuestName;
-				cout << "你的操作ID为："<<guestRandomId<<" 请牢记！！"<<endl;
+				cout << "你的操作ID为：" << guestRandomId << " 请牢记！！" << endl;
 				headguest = new guestchain(guestRandomId, GuestName);					//初始化游客单链表结点，头指针指向当前单链表
 				cout << "请输入景点代码：";
 				cin >> SightID;
-				SToperatepoint = MainSightTreePoint->search(SightID, STSearchpoint);	//查找景点代码在树中位置，返回指针
-				STSearchpoint = MainSightTreePoint;										//搜索指针归位
+				//SToperatepoint = MainSightTreePoint->search(SightID, STSearchpoint);	//查找景点代码在树中位置，返回指针
+				//STSearchpoint = MainSightTreePoint;										//搜索指针归位
 
-				if (SToperatepoint->chain == NULL) {									//如果当前景点下无人预约
-					SToperatepoint->chain = headguest;									//则将当前生成的单链表头结点连接到树中
+				OperatePoint = MainSightAVLTreePoint->search(SightID);
+
+				if (OperatePoint->chain == NULL) {									//如果当前景点下无人预约
+					OperatePoint->chain = headguest;									//则将当前生成的单链表头结点连接到树中
 					cout << "预约成功" << endl;
 				}
 				else
 				{
-					GCoperatepoint = SToperatepoint->chain;															//如果当前景点下已有单链表，单链表操作指针指向当前树下结点
-					SToperatepoint->chain->insertchain(guestRandomId, GuestName, GCoperatepoint);					//插入当前景点下单链表
+					GCoperatepoint = OperatePoint->chain;															//如果当前景点下已有单链表，单链表操作指针指向当前树下结点
+					OperatePoint->chain->insertchain(guestRandomId, GuestName, GCoperatepoint);					//插入当前景点下单链表
 					cout << "预约成功" << endl;
 				}
-				SToperatepoint->pv++;
-				SToperatepoint = MainSightTreePoint;									//操作指针归位
-				AllTreePrint(MainSightTreePoint);
+				OperatePoint->pv++;
+
 			}
 			else if (OperateId == 3) {
 				cout << "请输入了景点代码：";
 				cin >> SightID;
 				cout << "请输入你的操作ID：";
 				cin >> guestRandomId;
-				SToperatepoint = MainSightTreePoint->search(SightID, STSearchpoint);	//查找景点代码在树中位置，返回指针
-				STSearchpoint = MainSightTreePoint;										//搜索指针归位
-				if (SToperatepoint->chain == NULL) {									//如果当前景点下无人预约
+				//SToperatepoint = MainSightTreePoint->search(SightID, STSearchpoint);	//查找景点代码在树中位置，返回指针
+				//STSearchpoint = MainSightTreePoint;										//搜索指针归位
+				OperatePoint = MainSightAVLTreePoint->search(SightID);
+
+				if (OperatePoint->chain == NULL) {									//如果当前景点下无人预约
 					cout << "无预约信息" << endl;
 				}
 				else
 				{
-					GCoperatepoint = SToperatepoint->chain;														//如果当前景点下已有单链表，单链表操作指针指向当前树下结点
-					SToperatepoint->chain=SToperatepoint->chain->deletechain(guestRandomId, GCoperatepoint);							//删除当前景点下单链表
+					GCoperatepoint = OperatePoint->chain;														//如果当前景点下已有单链表，单链表操作指针指向当前树下结点
+					OperatePoint->chain = OperatePoint->chain->deletechain(guestRandomId, GCoperatepoint);							//删除当前景点下单链表
 				}
-				SToperatepoint->pv--;
-				SToperatepoint = MainSightTreePoint;									//操作指针归位
-				AllTreePrint(MainSightTreePoint);
+
 			}
 			else if (OperateId == 4)
 			{
@@ -132,12 +146,12 @@ int main() {
 			}
 		}
 		//退出
-		while (UserID ==3)
+		while (UserID == 3)
 		{
 			UILoop = 0;
 			break;
 		}
-		
+
 	}
 
 
